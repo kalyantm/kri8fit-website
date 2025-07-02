31f2110ef42ff20a0286b24811d46b4ba55abdd8
Kalyan TM <madabhushi.kalyan@gmail.com>
2025-07-02 21:21:18 +0530

    fix: simplify email flow

--- app/auth/confirm/page.tsx
+++ app/auth/confirm/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import PageIllustration from '@/components/page-illustration'
-import { supabase } from '@/supabase';
 import { useSearchParams } from 'next/navigation';
 import { Suspense, useEffect, useState } from 'react'
 
@@ -13,19 +12,12 @@ function ConfirmScreen() {
   const token_hash = searchParams.get('token_hash');
 
   useEffect(() => {
-    if (!token_hash) 
-        setConfirmationText('Verification failed. Please reach out to support@kri8.fit.');
-
-    const confirm = async () => {
-      const { error } = await supabase.auth.exchangeCodeForSession(token_hash as string);
-      if (error) {
-        setConfirmationText('Verification failed. Please reach out to support@kri8.fit.');
-      } else {
-        setConfirmationText('Email confirmed successfully');
-      }
-    };
-
-    confirm().finally(() => setLoading(false));
+    if (!token_hash) {
+      setConfirmationText('Email verification failed. Please reach out to support@kri8.fit.');
+    } else {
+      setConfirmationText('Your email has been confirmed successfully!');
+    }
+    setLoading(false)
   }, [token_hash]);
 
   return (
@@ -36,7 +28,9 @@ function ConfirmScreen() {
       <section className="relative">
         <div className="max-w-6xl mx-auto px-4 sm:px-6 relative">
           <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-            <h2 className="h1 font-red-hat-display mb-4">Email confirmation</h2>
+            <h2 className="h1 font-red-hat-display mb-4">
+              Email confirmation
+            </h2>
             {loading ? (
               <p className="text-lg space-y-2 text-gray-700 dark:text-gray-400 mb-8">
                 Loading...
@@ -46,7 +40,7 @@ function ConfirmScreen() {
               {confirmationText}
             </p>
             )}
-          {confirmationText.includes('Confirmed') && (
+          {confirmationText.includes('confirmed') && (
             <a href="kri8.fit://kri8.fit/login" className="btn-sm text-white dark:text-black bg-[#85f193] hover:bg-[#85f193] dark:bg-[#85f193] dark:hover:bg-[#85f193]">
               Go to kri8.fit app
             </a>
